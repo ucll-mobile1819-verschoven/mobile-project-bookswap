64f48a79ade5fb8ce6d51dfba0afc9c0716898eb
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-04-15 19:31:42 +0200

    navigation

--- SwapBook/App.js
+++ SwapBook/App.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+        <Text>Home</Text>
+        <Button title="Profile" onPress={() => this.props.navigation.navigate('Profile')}/>
+        <Button title="Home" onPress={() => this.props.navigation.push('Home')}/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class ProfileScreen extends React.Component {
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text>Profile</Text>
+      <Button title="Home" onPress={() => this.props.navigation.navigate('Home')}/>
+      <Button title="Back" onPress={() => this.props.navigation.goBack()}/>
+    </View>
+    );
+  }
+}
+
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Profile: ProfileScreen,  
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component{
+  render()
+{
+  return <AppContainer />;
+}}
+
+
