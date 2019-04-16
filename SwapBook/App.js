e6eccb29461eb1995ae21d82e0dcbb823c2562ea
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-04-16 22:13:29 +0200

    map + get user and seller location (seller location on temp database)

--- SwapBook/App.js
+++ SwapBook/App.js
@@ -1,49 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
-class HomeScreen extends React.Component {
+
+import FetchLocation from './components/FetchLocation'
+import UsersMap from './components/UsersMap';
+
+
+//Dit moet gebruikt worden in de paginas waar een map nodig is
+//Dit is dus niet de hoofdpagina natuurlijk
+export default class App extends React.Component{
+  state = {
+    userLocation: null,
+    sellerLocation: []
+  }
+
+  //get user location
+  getUserLocationHandler = () => {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({
+          userLocation: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }
+        });
+        //SET location seller of book         => Will need it with making an acount
+       /* fetch('https://swapbook-2403f.firebaseio.com/seller.json', {
+          method: 'POST',
+          body: JSON.stringify({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          })
+        })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+*/
+      }, err => console.log(err));
+  }
+  getSellerLocationHandler = () => {
+      //GET location seller of book
+      fetch('https://swapbook-2403f.firebaseio.com/seller.json')
+      .then(response => response.json())
+      .then(parsedResponse => {
+        const locationArray = [];
+        for (const key in parsedResponse){
+          locationArray.push({
+            latitude: parsedResponse[key].latitude,
+            longitude: parsedResponse[key].longitude,
+            id: key
+          })
+        }
+        this.setState({
+          sellerLocation: locationArray
+        });
+      })
+      .catch(err => console.log(err));
+    }
+
   render() {
     return (
-      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <Text>Home</Text>
-        <Button title="Profile" onPress={() => this.props.navigation.navigate('Profile')}/>
-        <Button title="Home" onPress={() => this.props.navigation.push('Home')}/>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Button title="Get Seller Location" onPress={this.getSellerLocationHandler} style={styles.flex1}/>
+          <FetchLocation onGetLocation={this.getUserLocationHandler} style={styles.flex2}/>
+        </View>
+        <UsersMap userLocation={this.state.userLocation} sellerLocation={this.state.sellerLocation} style={styles.flex3}/>
       </View>
     );
   }
 }
 
-class ProfileScreen extends React.Component {
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>Profile</Text>
-      <Button title="Home" onPress={() => this.props.navigation.navigate('Home')}/>
-      <Button title="Back" onPress={() => this.props.navigation.goBack()}/>
-    </View>
-    );
-  }
-}
-
-
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Profile: ProfileScreen,  
+const styles = StyleSheet.create({
+  container: {
+    marginTop: '20%',
+    backgroundColor: "#ffe",
   },
-  {
-    initialRouteName: 'Home',
-  }
-);
-
-const AppContainer = createAppContainer(RootStack);
-
-export default class App extends React.Component{
-  render()
-{
-  return <AppContainer />;
-}}
-
-
+  header: {
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  flex1: {
+    flex: 1,
+    backgroundColor: 'powderblue',
+  },
+  flex2: {
+    flex: 2,
+    backgroundColor: 'skyblue'
+  },
+  flex3: {
+    flex: 3,
+    backgroundColor: 'steelblue'
+  },
+});
---
+++ SwapBook/components/UsersMap.js
@@ -0,0 +1,277 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
+
+const usersMap = props => {
+    let userLocationMarker = null;
+    if(props.userLocation) {
+        userLocationMarker= <MapView.Marker coordinate={props.userLocation}/>;
+    }
+
+    const sellerLocationMarker = props.sellerLocation.map(sellerLocation => (
+        <MapView.Marker coordinate={sellerLocation} key={sellerLocation.Id}/>
+    ));
+
+    return (
+        <View styles={styles.mapContainer}>
+            <MapView region={props.userLocation} style={styles.map} customMapStyle={mapStyle}> 
+               {userLocationMarker}
+               {sellerLocationMarker}
+            </MapView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    mapContainer: {
+        width: '100%',
+        height: 150,
+        marginTop: 20
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+    mapStyle:{
+            
+    }
+});
+
+//alternate map style for fun
+const mapStyle = StyleSheet.create([
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#1d2c4d"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#8ec3b9"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#1a3646"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.country",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#4b6878"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#64779e"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.province",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#4b6878"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#334e87"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape.natural",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#023e58"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#283d6a"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#6f9ba5"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#1d2c4d"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#023e58"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#3C7680"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#304a7d"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#98a5be"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#1d2c4d"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#2c6675"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#255763"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#b0d5ce"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#023e58"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#98a5be"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#1d2c4d"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#283d6a"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#3a4762"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#0e1626"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#4e6d70"
+        }
+      ]
+    }
+  ])
+
+
+
+export default usersMap
