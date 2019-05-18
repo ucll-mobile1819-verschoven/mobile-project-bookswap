c703d02b5f90cc986a52f6ab4516a16f17342c0c
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-05-18 18:08:46 +0200

    password not visible

--- SwapBook/src/screens/RegisterScreen.js
+++ SwapBook/src/screens/RegisterScreen.js
@@ -34,6 +34,8 @@ const options = {
       error: 'Without an email address how are you going to reset your password when you forget it?'
     },
     password: {
+      password: true,
+secureTextEntry: true,
       error: 'Choose something you use on a dozen other sites or something you won\'t remember.'
     },
   }
