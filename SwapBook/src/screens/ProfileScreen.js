7746cfe8804a32b0410a1f988b06773c47cf58de
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-05-30 21:08:35 +0200

    added profile with session

--- SwapBook/src/screens/ProfileScreen.js
+++ SwapBook/src/screens/ProfileScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, AsyncStorage } from 'react-native';
 import {Header} from 'react-native-elements';
 import Logo from '../components/Logo';
 import { styles } from '../styles/Style';
+import { db } from '../config/db';
 
+let usersRef = db.ref('/seller');
 
 export default class ProfileScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        session: '',
+        users: [],
+    };
+}
     static navigationOptions = ({ navigation, navigationOptions }) => {
       const { params } = navigation.state;
   
@@ -19,14 +28,61 @@ export default class ProfileScreen extends React.Component {
       //<Button title="Update the title" onPress={() => this.props.navigation.setParams({otherParam: 'Updated!'}) } color="#0BB586"/>
       };
     };
-    render() {
-      return (
-        <View>
-          <Header leftComponent={ <Logo/> } centerComponent={{ text: 'My Profile', style: { color: '#fff' } }} containerStyle={{backgroundColor:'#0BB586'}  }/>
+    logout = async () => {
+      try {
+        await AsyncStorage.removeItem('@email');
+        this.props.navigation.navigate('Welcome');
+      } catch(e) {
+        alert(e.message);
+      }
+    }
+
+    getEmail = async () => {
+      try {
+        const value = await AsyncStorage.getItem('@email');
+        this.setState({session : value})
+      } catch(e) {
+        alert(e.message);
+      }
+    }
         
-          <Text>Profile Screen</Text>
+    //GET BOOKS
+    componentDidMount() {
+        this.getEmail();
+
+        usersRef.on('value', (snapshot) => {
+          let data = snapshot.val();
+          let users = Object.values(data);
+          this.setState({ users });
+        });
+
+        
+    }
+
+    render() {
+      var user = [];
+      for  (let i=0; i< this.state.users.length; i++) {
+        if (this.state.session == this.state.users[i].email) {
+          let email = this.state.users[i].email;
+          let firstname = this.state.users[i].firstname;
+          let lastname = this.state.users[i].lastname;
+          let residence = this.state.users[i].residence;
+          user.push(
+            <View style={styles.books} key={"book" + i}>
+              <Text>Email: {email}</Text>
+              <Text>First name: {firstname}</Text>
+              <Text>Last name: {lastname}</Text>
+              <Text>Residence: {residence}</Text>
+            </View>
+          )
+        } 
+      }
+      return (
+        <View style={styles.centered}>
+          <Header leftComponent={ <Logo/> } centerComponent={{ text: 'My Profile', style: { color: '#fff' } }} containerStyle={{backgroundColor:'#0BB586'}  }/>
          
-          <Button title="Logout" onPress={() => this.props.navigation.navigate('Welcome') } color="#0BB586"/>
+          <View>{user}</View>
+          <Button title="Logout" onPress={this.logout} color="#0BB586"/>
         </View>
   
       );
