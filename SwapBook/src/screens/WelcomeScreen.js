d80a46c0dc246b7dedb32c2dc536e398b65311cd
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-05-30 14:06:02 +0200

    welcomescreen in progress

--- SwapBook/src/screens/WelcomeScreen.js
+++ SwapBook/src/screens/WelcomeScreen.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { Text, TextInput, View, Button } from 'react-native';
 import {Header} from 'react-native-elements';
-import { styles } from '../styles/Style';
 import Logo from '../components/Logo';
-import { TextInput } from 'react-native-paper';
+import { styles } from '../styles/Style';
+import { db } from '../config/db';
+
+let usersRef = db.ref('/seller');
 
 export default class WelcomeScreen extends React.Component{  
+  state = { users: [], email: '', password: '', errorMessage: null }
+
+  handleLogin = () => {
+    for (let user of this.state.users) {
+      //TODO: pass sellerId property to homescreen 
+        if (this.state.email == user.email && this.state.password == user.password) {
+          this.props.navigation.navigate('Tabnav', {sellerId: user.email});
+          break;
+        } else {
+        this.state.errorMessage= "Incorrect email or password";
+        }
+    }
+  }
+
+  componentDidMount() {
+    usersRef.on('value', (snapshot) => {
+      let data = snapshot.val();
+      let users = Object.values(data);
+      this.setState({ users });
+    });
+  }
+
   render(){
     let display = "none"
     const { params } = this.props.navigation.state;
@@ -13,7 +37,8 @@ export default class WelcomeScreen extends React.Component{
     if (success!=null){
         display = "flex"
     }
-//const display = success ? "none" : "flex";
+
+
 
     return(
       <View>
@@ -22,9 +47,37 @@ export default class WelcomeScreen extends React.Component{
        {/*<Text style={{display}}>{JSON.stringify(success)}</Text>*/}
        <View style={{display}}><Button title="Great job! You can now login!" onPress={() => this.props.navigation.navigate('Tabnav')} color='#0BB586' backgroundColor='transparant'/></View>
 
+        <View style={styles.formStyle}>
+       <Text>Login</Text>
+        {this.state.errorMessage &&
+          <Text style={{ color: 'red' }}>
+            {this.state.errorMessage}
+          </Text>}
+        <TextInput
+          style={styles.textInput}
+          autoCapitalize="none"
+          placeholder="Email"
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+        />
+        <TextInput
+          secureTextEntry
+          style={styles.textInput}
+          autoCapitalize="none"
+          placeholder="Password"
+          onChangeText={password => this.setState({ password })}
+          value={this.state.password}
+        />
+
+</View>
         <Button
           title="Login"
-          onPress={() => this.props.navigation.navigate('Login')}
+          onPress={() => this.props.navigation.navigate(this.handleLogin)}
+          color="#0BB586"
+        />
+        <Button
+          title="Login without logging in"
+          onPress={() => this.props.navigation.navigate('Tabnav')}
           color="#0BB586"
         />
          <View style={{marginTop: 5, borderRadius: 5}}>
