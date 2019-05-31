41095fee4949483891de14239e39a76f52184cc5
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-05-31 11:22:56 +0200

    data of clicked book

--- SwapBook/src/screens/BookScreen.js
+++ SwapBook/src/screens/BookScreen.js
@@ -3,11 +3,39 @@ import { Text, View, Button, ScrollView } from 'react-native';
 import {Header} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Ionicons'
 import { styles } from '../styles/Style';
+import { db } from '../config/db';
 
+
+let itemsRef = db.ref('/book');
 
 export default class BookScreen extends React.Component {
   
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        items: [],
+        sellerId: '',
+        titleBook: '',
+    };
+  };
+  componentDidMount(){
  
+    
+
+    const { params } = this.props.navigation.state;
+    const title = params ? params.title : null;
+    const sellerId = params ? params.sellerId : null;
+
+    this.setState({titleBook : title});
+    this.setState({sellerId : sellerId});
+
+    itemsRef.on('value', (snapshot) => {
+      let data = snapshot.val();
+      let items = Object.values(data);
+      this.setState({ items });
+    })
+  }
 
 
   static navigationOptions = {
@@ -23,27 +51,38 @@ export default class BookScreen extends React.Component {
     //here get all data of the book by the id of the book???
   };
   render(){
-    const { params } = this.props.navigation.state;
-    const itemId = params ? params.itemId : null;
-    const otherParam = params ? params.otherParam : null;
-    const title = params ? params.title : null;
-    const sellerId = params ? params.sellerId : null;
+    var item = [];
+      for  (let i=0; i< this.state.items.length; i++) {
+        if (this.state.sellerId == this.state.items[i].sellerId && this.state.titleBook == this.state.items[i].title) {
+          let author = this.state.items[i].author;
+          let condition = this.state.items[i].condition;
+          let isbn = this.state.items[i].isbn;
+          let price = this.state.items[i].price;
+          let sellerId = this.state.items[i].sellerId;
+          let title = this.state.items[i].title;
+          item.push(
+            <View style={styles.books} key={"bookinfo" + i}>
+              <Text>author: {author}</Text>
+              <Text>condition: {condition}</Text>
+              <Text>isbn: {isbn}</Text>
+              <Text>price: {price}</Text>
+              <Text>sellerId: {sellerId}</Text>
+              <Text>title: {title}</Text>
+            </View>
+          )
+          break;
+        } 
+      }
     return (
       <View>
         <Header leftComponent={ <Icon name="ios-arrow-back" color={'#fff'} size={30} onPress={()=> this.props.navigation.goBack()} title='Go Back'/> } 
                 centerComponent={{ text: 'Info Book', style: { color: '#fff' } }} 
                 containerStyle={{backgroundColor:'#0BB586'}}/>        
-        <Text>Info of the seller will be put on this page</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Text>Title Book: {JSON.stringify(title)}</Text>
-        <Text>Seller Id: {JSON.stringify(sellerId)}</Text>
-        <Text>Name Book</Text>
-        <Text>Name of the Seller</Text>
-        <Text>Price</Text>
+      
         
+        <View>{item}</View>
 
-        <Button title='seller' onPress={() => this.props.navigation.navigate('SellerProfile', {sellerId:  sellerId}) } color="#0BB586"/>
+        <Button title='seller' onPress={() => this.props.navigation.navigate('SellerProfile', {sellerId:  this.state.sellerId}) } color="#0BB586"/>
       </View>
     );
   }
