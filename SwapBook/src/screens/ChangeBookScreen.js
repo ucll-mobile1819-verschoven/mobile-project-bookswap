1608c99cba4ba8752db5f5139aa812126050fb72
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-05-31 18:14:30 +0200

    update a book with param

--- SwapBook/src/screens/ChangeBookScreen.js
+++ SwapBook/src/screens/ChangeBookScreen.js
@@ -4,9 +4,10 @@ import {Header} from 'react-native-elements';
 import { styles } from '../styles/Style';
 import Logo from '../components/Logo';
 import t from 'tcomb-form-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
+import Icon from 'react-native-vector-icons/Ionicons'
 import { db } from '../config/db';
+
 
 let itemsRef = db.ref('book/');
 const Form = t.form.Form;
@@ -24,22 +25,25 @@ const Book = t.struct({
 const options = {
   fields: {
     title: {
-      error: 'What is the title of the book?'
+      error: 'What is the title of the book?',
     },
     author: {
-      error: 'Who wrote the book?'
+      error: 'Who wrote the book?',
     },
     condition: {
-      error: 'Is your book as new or used?'
+      error: 'Is your book as new or used?',
+      
     },
     isbn: {
-      error: 'Every book has an ISBN'
+      error: 'Every book has an ISBN',
     },
     price: {
-      error: 'You will not sell your book for free, will you?'
+      error: 'You will not sell your book for free, will you?',
     }
   }
 }
+
+
 
 export default class ChangeBookScreen extends React.Component{  
   constructor(props) {
@@ -47,7 +51,6 @@ export default class ChangeBookScreen extends React.Component{
     this.state = {
         session: '',
         item: [],
-        items: [],
         itemId: '',
     };
   }
@@ -73,18 +76,11 @@ export default class ChangeBookScreen extends React.Component{
   }
   handleSubmitDelete = () => {
     let removeRef = db.ref('book/' + this.state.itemId);
-    removeRef.remove()
+    removeRef.remove();
     this.props.navigation.navigate('MyBookScreen', {session:  this.state.session});
   }
         
   componentDidMount() {
-    itemsRef.on('value', (snapshot) => {
-      let data = snapshot.val();
-      let items = Object.values(data);
-      this.setState({ items });
-      
-    });
-
     const { params } = this.props.navigation.state;
     const session = params ? params.session : null;
     this.setState({session : session});
@@ -98,13 +94,21 @@ export default class ChangeBookScreen extends React.Component{
 }
 
   render(){
+    const values = {
+      title: this.state.item.title,
+      author: this.state.item.author,
+      condition: this.state.item.condition,
+      isbn: this.state.item.isbn,
+      price: this.state.item.price,
+    }
     return(
       <KeyboardAvoidingView style={styles.centered} style={styles.container} behavior="padding" enabled>
-         <Header leftComponent={ <Logo/> } centerComponent={{ title: 'Edit your book', style: { color: '#fff' }}} containerStyle={{backgroundColor:'#0BB586',}}/>
+         <Header  leftComponent={ <Icon name="ios-arrow-back" color={'#fff'} size={30} onPress={()=> this.props.navigation.goBack()} title='Go Back'/> } 
+                  centerComponent={{ text: 'Edit your book', style: { color: '#fff' }}} containerStyle={{backgroundColor:'#0BB586',}}/>
         <ScrollView>
           <View>
                 <View style={styles.formStyle}>
-                  <Form ref={c => this._form = c} type={Book} options={options}/>
+                  <Form ref={c => this._form = c} type={Book} options={options} value={values}/>
                   <Button title="Update book" onPress={this.handleSubmitUpdate} color="#0BB586"/>
                   <Button title="Delete book" onPress={this.handleSubmitDelete} color="#0BB586"/>
 	      	      </View>
