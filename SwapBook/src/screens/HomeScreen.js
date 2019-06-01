d22ab47528d622bc86e8fa6a275075268ae5cf35
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-06-01 17:35:28 +0200

    layout homescreen fixed + logout button + removed comments

--- SwapBook/src/screens/HomeScreen.js
+++ SwapBook/src/screens/HomeScreen.js
@@ -10,63 +10,23 @@ let itemsRef = db.ref('/book');
 
 
 export default class HomeScreen extends React.Component{  
-  
-
-
- // static navigationOptions = ({navigation}) => {
- //   const params = navigation.state.params || {};
-   // return {
-
-      //in comments voorbeeld voor params mee te geven
-    /*  headerRight: (
-        <Button onPress={params.increaseCount} title="+1" color="#eee" />
-      ),*/
-  //  }
- // };
-
-  /*componentWillMount() {
-    this.props.navigation.setParams({ increaseCount: this._increaseCount });
-  }
-  state = {
-    count: 0,
-  };
-  _increaseCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  };*/
 
   constructor(props) {
     super(props);
-
-    // Met state kan er data bijgehouden worden binnen het component
     this.state = {
         items: [],
         session: '',
     };
-
-   
-    // functie initialiseren - alleen bij functies waarvoor this. wordt gebruikt
-  //this.loadData = this.loadData.bind(this);
-    //this.getAllBooks = this.getAllBooks.bind(this);
-
 }
-/*
-loadData() {
-    // GET request naar de URL returned een lijst van students in een classgroup
-    
-    // BELANGRIJK: ZORG DAT DE BACKEND DRAAIT op poort 8080 - mogelijk moet je het ip adres aanpassen naar die van jou pc
-    axios.get("http://192.168.137.1:8080/books")
-        // .then - bij een 200 (OK)
-        // krijgt een response mee: JSON
-        .then((response) => {
-            // Wanneer OK dan voert hij alles hierbinnen uit
-            this.setState({
-                // response.data is de JSON uit de back-end
-                data: response.data
-            });
-        });
-}
-*/
 
+logout = async () => {
+  try {
+    await AsyncStorage.removeItem('@email');
+    this.props.navigation.navigate('Welcome');
+  } catch(e) {
+    alert(e.message);
+  }
+}
 getEmail = async () => {
   try {
     const value = await AsyncStorage.getItem('@email');
@@ -105,26 +65,29 @@ componentDidMount() {
     }
     return(
       <View style={styles.centered}>
-        <Header leftComponent={ <Logo/> } centerComponent={{ text: 'SwapBook', style: { color: '#fff',}}} containerStyle={{backgroundColor:'#0BB586'}}/>
-        
-          <ScrollView style={{marginBottom: 5, paddingBottom: 5, maxHeight: "55%"}}>{ books}</ScrollView>
-          <View style={styles.centered}>
-          <Button 
-          buttonStyle={
-            {            
-              width:'100%',
-              marginTop:15,
-              backgroundColor:"#0BB586"
-            }
-          }
-          type="solid" 
-          title="AddBook" 
-          onPress={() => this.props.navigation.navigate('AddBook')} 
-          />
-             
-          </View>
-         
-      </View>
+
+        <Header leftComponent={ <Logo/> } 
+                centerComponent={{ text: 'SwapBook', style: { color: '#fff',}}} containerStyle={{backgroundColor:'#0BB586'}}
+                rightComponent={<Button titleStyle={{color:"white"}} type="clear" title="Logout" onPress={this.logout} color="white"/>}/>
+ 
+          <View style={{flexGrow:1}}>
+            <ScrollView style={{maxHeight:'80%'}}>{ books}</ScrollView>
+            <View style={styles.centered}>
+              <Button 
+              buttonStyle={
+                {            
+                  width:'100%',
+                  marginTop:15,
+                  backgroundColor:"#0BB586"
+                }
+              }
+              type="solid" 
+              title="AddBook" 
+              onPress={() => this.props.navigation.navigate('AddBook')} 
+              />
+            </View>
+         </View>
+        </View>
     )
   }
 };
