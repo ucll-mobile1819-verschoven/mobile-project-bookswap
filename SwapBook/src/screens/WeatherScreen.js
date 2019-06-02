52996fc86eecc4b6a0c00a56a640dd9bd66eb5bc
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-06-02 15:16:39 +0200

    changed api key

--- SwapBook/src/screens/WeatherScreen.js
+++ SwapBook/src/screens/WeatherScreen.js
@@ -10,9 +10,9 @@ export default class MyBookScreen extends React.Component {
         super(props);
         const { params } = this.props.navigation.state;
         const residence = params ? params.residence : null;
-        
+        this.setState({homecity : residence});
         this.state = {
-            homecity: residence,
+            homecity : '',
             weatherData: {
               coord: [],
               weather: [{
@@ -35,48 +35,45 @@ export default class MyBookScreen extends React.Component {
             }
         };
     }
+
     getWeatherData = () =>{
-        axios.get("https://api.openweathermap.org/data/2.5/weather?q="+ this.state.homecity +"&appid=4461c8af39efae2ba9b252bc7bad0390&units=metric")
+        console.log('GET WEATHER DATA: ' + this.state.homecity);
+        axios.get("https://api.openweathermap.org/data/2.5/weather?q="+ this.state.homecity +"&appid=7a041a9cabf3c0a1169a2a9fb5d1e889&units=metric")
         .then((response) => {
             this.setState({
             weatherData : response.data})
         })
       }
-      componentDidMount(){
-          this.getWeatherData();
-      }
 
+  render(){ 
+    return (
+      <View style={{flex: 1}} >
+         <Header leftComponent={ <Icon name="ios-arrow-back" color={'#fff'} size={30} onPress={()=> this.props.navigation.goBack()} title='Go Back'/> } 
+                centerComponent={{ text: 'Info Book', style: { color: '#fff' } }} 
+                containerStyle={{backgroundColor:'#0BB586'}}/>            
 
-    render(){ 
-        return (
-        <View style={{flex: 1}} >
-            <Header leftComponent={ <Icon name="ios-arrow-back" color={'#fff'} size={30} onPress={()=> this.props.navigation.goBack()} title='Go Back'/> } 
-                    centerComponent={{ text: 'Info Book', style: { color: '#fff' } }} 
-                    containerStyle={{backgroundColor:'#0BB586'}}/>            
-
-                <Input  
-                label={'City'}  
-                value={this.state.homecity}       
-                onChangeText={place => this.setState({ homecity: place })}
-                />
-                <Button
-                buttonStyle={
-                {
-                    marginTop:15,
-                    backgroundColor:"#0BB586"
-                }
-                }
-                type="solid"
-                title="search"
-                
-                onPress={this.getWeatherData}
+            <Input  
+            label={'City'}         
+            value={this.state.homecity}
+            onChangeText={place => this.setState({ homecity: place })}
             />
-            <Text>{this.state.weatherData.name}, {this.state.weatherData.sys.country}</Text>
-            <Text>{this.state.weatherData.main.temp} °C </Text>
-            <Text>{this.state.weatherData.weather[0].main} - {this.state.weatherData.weather[0].description} </Text>
-            
-            </View>
-        );
+            <Button
+            buttonStyle={
+              {
+                marginTop:15,
+                backgroundColor:"#0BB586"
+              }
+            }
+            type="solid"
+            title="search"
+            onPress={this.getWeatherData}
+          />
+
+          <Text>{this.state.weatherData.name}, {this.state.weatherData.sys.country}</Text>
+          <Text>{this.state.weatherData.main.temp} °C </Text>
+          <Text>{this.state.weatherData.weather[0].main} - {this.state.weatherData.weather[0].description} </Text>
+
+        </View>
+    );
   }
 }
-
