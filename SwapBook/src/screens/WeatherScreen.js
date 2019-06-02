6306e487bbb0ea0cfff0e4972a033781ab61ea60
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-06-02 13:28:29 +0200

    residence coupled with weather

--- SwapBook/src/screens/WeatherScreen.js
+++ SwapBook/src/screens/WeatherScreen.js
@@ -8,8 +8,11 @@ import axios from 'axios';
 export default class MyBookScreen extends React.Component {
     constructor(props) {
         super(props);
+        const { params } = this.props.navigation.state;
+        const residence = params ? params.residence : null;
+        
         this.state = {
-            residence : '',
+            homecity: residence,
             weatherData: {
               coord: [],
               weather: [{
@@ -32,51 +35,48 @@ export default class MyBookScreen extends React.Component {
             }
         };
     }
-    componentDidMount() {
-        const { params } = this.props.navigation.state;
-        const residence = params ? params.residence : null;
-        console.log(residence);
-        this.setState({residence : residence});
-        this.getWeatherData;
-    }
-    
     getWeatherData = () =>{
-        axios.get("https://api.openweathermap.org/data/2.5/weather?q="+ this.state.residence +"&appid=4461c8af39efae2ba9b252bc7bad0390&units=metric")
+        axios.get("https://api.openweathermap.org/data/2.5/weather?q="+ this.state.homecity +"&appid=4461c8af39efae2ba9b252bc7bad0390&units=metric")
         .then((response) => {
             this.setState({
             weatherData : response.data})
         })
       }
+      componentDidMount(){
+          this.getWeatherData();
+      }
 
-  render(){ 
-    return (
-      <View style={{flex: 1}} >
-         <Header leftComponent={ <Icon name="ios-arrow-back" color={'#fff'} size={30} onPress={()=> this.props.navigation.goBack()} title='Go Back'/> } 
-                centerComponent={{ text: 'Info Book', style: { color: '#fff' } }} 
-                containerStyle={{backgroundColor:'#0BB586'}}/>            
 
-            <Input  
-            label={'City'}         
-            onChangeText={place => this.setState({ residence: place })}
+    render(){ 
+        return (
+        <View style={{flex: 1}} >
+            <Header leftComponent={ <Icon name="ios-arrow-back" color={'#fff'} size={30} onPress={()=> this.props.navigation.goBack()} title='Go Back'/> } 
+                    centerComponent={{ text: 'Info Book', style: { color: '#fff' } }} 
+                    containerStyle={{backgroundColor:'#0BB586'}}/>            
+
+                <Input  
+                label={'City'}  
+                value={this.state.homecity}       
+                onChangeText={place => this.setState({ homecity: place })}
+                />
+                <Button
+                buttonStyle={
+                {
+                    marginTop:15,
+                    backgroundColor:"#0BB586"
+                }
+                }
+                type="solid"
+                title="search"
+                
+                onPress={this.getWeatherData}
             />
-            <Button
-            buttonStyle={
-              {
-                marginTop:15,
-                backgroundColor:"#0BB586"
-              }
-            }
-            type="solid"
-            title="search"
-            onPress={this.getWeatherData}
-          />
-
-          <Text>{this.state.weatherData.name}, {this.state.weatherData.sys.country}</Text>
-          <Text>{this.state.weatherData.main.temp} °C </Text>
-          <Text>{this.state.weatherData.weather[0].main} - {this.state.weatherData.weather[0].description} </Text>
-          
-        </View>
-    );
+            <Text>{this.state.weatherData.name}, {this.state.weatherData.sys.country}</Text>
+            <Text>{this.state.weatherData.main.temp} °C </Text>
+            <Text>{this.state.weatherData.weather[0].main} - {this.state.weatherData.weather[0].description} </Text>
+            
+            </View>
+        );
   }
 }
 
