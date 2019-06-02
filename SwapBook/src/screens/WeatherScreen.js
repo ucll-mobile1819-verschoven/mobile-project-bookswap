a41e9675275d00c17c9ec11039ede470a1c8aed4
Daan Lemmens <daan.lemmens@student.ucll.be>
2019-06-02 16:56:59 +0200

    weather without button

--- SwapBook/src/screens/WeatherScreen.js
+++ SwapBook/src/screens/WeatherScreen.js
@@ -12,6 +12,10 @@ export default class MyBookScreen extends React.Component {
         const { params } = this.props.navigation.state;
             const homecity = params ? params.homecity : null;
         this.state = {
+            //currentColor: 'rgba(255,255,255,0.5',
+           // nextColor: this._randomColor(),
+            //icon: weatherIcon()
+
             homecity : homecity,
             weatherData: {
               coord: [],
@@ -51,28 +55,20 @@ export default class MyBookScreen extends React.Component {
     return (
       <View style={{flex: 1}} >
          <Header leftComponent={ <Icon name="ios-arrow-back" color={'#fff'} size={30} onPress={()=> this.props.navigation.goBack()} title='Go Back'/> } 
-                centerComponent={{ text: 'Info Book', style: { color: '#fff' } }} 
+                centerComponent={{ text: 'Weather', style: { color: '#fff' } }} 
                 containerStyle={{backgroundColor:'#0BB586'}}/>            
 
             <Input  
             label={'City'}         
             value={this.state.homecity}
             onChangeText={place => this.setState({ homecity: place })}
-            />
-            <Button
-            buttonStyle={
-              {
-                marginTop:15,
-                backgroundColor:"#0BB586"
-              }
-            }
-            type="solid"
-            title="search"
-            onPress={this.getWeatherData}
-          />
-          <Text>{this.state.weatherData.name}, {this.state.weatherData.sys.country}</Text>
-          <Text>{this.state.weatherData.main.temp} °C </Text>
-          <Text>{this.state.weatherData.weather[0].main} - {this.state.weatherData.weather[0].description} </Text>
+            returnKeyType="go"
+            onSubmitEditing={this.getWeatherData}/>
+            
+            <Text>ICON: {this.state.weatherData.weather[0].main}</Text>
+            <Text>{this.state.weatherData.main.temp} °C </Text>
+            <Text>{this.state.weatherData.name}, {this.state.weatherData.sys.country}</Text>
+            <Text>{this.state.weatherData.weather[0].description}</Text>
 
         </View>
     );
